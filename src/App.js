0fa1e0c90dab664d53dd8d887a71169baaecc8ab
sanidhya70 <ksanidhya007@gmail.com>
2019-08-24 01:03:25 -0500

    Build Todo and TodoItem component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
 import "./App.css";
+import Todos from "./components/Todos";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        tittle: "Milk",
+        isCompleted: false
+      },
+      {
+        id: 2,
+        tittle: "bread",
+        isCompleted: false
+      },
+      {
+        id: 3,
+        tittle: "butter",
+        isCompleted: false
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.todo.tittle}</p>
+      </div>
+    );
+  }
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+};
+
+export default TodoItem;
